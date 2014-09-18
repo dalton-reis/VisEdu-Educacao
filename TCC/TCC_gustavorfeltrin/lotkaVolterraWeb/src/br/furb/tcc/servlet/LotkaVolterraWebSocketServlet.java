7d032e4cc9c30ce952dfc78022f071172a4700b3
Gustavo R. Feltrin <gustavo.feltrin@hotmail.com>
2014-09-17 21:25:48 -0300

    - Implementada execução de um agente (Jason) em um servidor se aplicação (TomCat); - Implementado agente no Jason que de forma reativa realiza suas ações na simulação;

--- TCC/TCC_gustavorfeltrin/lotkaVolterraWeb/src/br/furb/tcc/servlet/LotkaVolterraWebSocketServlet.java
+++ TCC/TCC_gustavorfeltrin/lotkaVolterraWeb/src/br/furb/tcc/servlet/LotkaVolterraWebSocketServlet.java
@@ -14,9 +14,9 @@ public class LotkaVolterraWebSocketServlet extends WebSocketServlet {
 
 	private static final long serialVersionUID = 1L;
 	
-	@Override
 	protected StreamInbound createWebSocketInbound(String subProtocol, HttpServletRequest request) {
-		return new ConnectionWS("");
+		String agentName = request.getParameter("agentName");
+		return new ConnectionWS( agentName );
 	}
 	
 }
--- TCC/TCC_gustavorfeltrin/lotkaVolterraWeb/src/br/furb/tcc/websocket/ConnectionWS.java
+++ TCC/TCC_gustavorfeltrin/lotkaVolterraWeb/src/br/furb/tcc/websocket/ConnectionWS.java
@@ -1,22 +1,26 @@
 package br.furb.tcc.websocket;
 
+import jason.asSyntax.Literal;
+
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.CharBuffer;
+import java.util.Arrays;
 
 import org.apache.catalina.websocket.MessageInbound;
 import org.apache.catalina.websocket.WsOutbound;
 
-import br.furb.tcc.agents.Prey;
-import br.furb.tcc.core.AgentManager;
-import br.furb.tcc.core.MensagemWS;
 import br.furb.tcc.log.Log;
+import br.furb.tcc.reasoning.Agente;
 
 @SuppressWarnings("deprecation")
 public class ConnectionWS extends MessageInbound {
 	
+	private String agentName;
+	
 	public ConnectionWS(String agentName) {
-		Log.sysout("ConnectionWS: ");
+		this.agentName = agentName;
+		Log.sysout("ConnectionWS: " + agentName);		
 	}
 	
 	@Override
@@ -29,30 +33,22 @@ public class ConnectionWS extends MessageInbound {
 		System.err.println( "Método não aceito" );
 	}
 	
-	private Prey agente;
-
 	@Override
 	protected void onTextMessage(CharBuffer msg) throws IOException {
-		Log.sysout("onTextMessage: " + msg);
-		MensagemWS mWs = MensagemWS.create( msg.toString() );
-		if (! AgentManager.get().containsAgent(mWs.getUuid())) {
-			/*Prey*/ agente = new Prey(mWs.getUuid(), this); // FIXME
-			AgentManager.get().addAgent(mWs.getUuid(), agente);
-		}
-		/**
-		 * Quando houver o que perceber adicionar. 
-		 */
-		// AgentManager.get().addPerception(mWs.getUuid(), mWs.getMensagem());
-		sendMessage( String.format("%s#%s#%s", mWs.getMensagem(), "green", "black") );
+		Log.sysout("onTextMessage: " + msg);		
+		String[] arrMsg = msg.toString().split("#");		
+		Agente ag = new Agente(agentName, Arrays.asList( Literal.parseLiteral( arrMsg[1] ) ), this);		
+		ag.run();
+//		sendMessage( String.format("%s#%s#%s", mWs.getMensagem(), "green", "black") );
 	}
 	
-	private void sendMessage(String message) {
+	public void sendMessage(String message) {
 		Log.sysout("sendMessage: " + message);
 		try {
 			getWsOutbound().writeTextMessage( CharBuffer.wrap(message) );
 		} catch (IOException e) {
 			e.printStackTrace();			
 		}
-	}		
+	}	
 
 }
