f892694740e6d072333427b69f00a917feb4c783
José Guilherme Vanz <guilherme.sft@gmail.com>
2015-03-07 15:26:49 -0300

    Script auxiliar para ROS
    Foi adicionado um script auxiliar para inicializar todo o ambiente
    necessário o funcionamento do VisEdu-Drone. Para que seja possível
    a comunicação entre o browser e o AR.Drone Parrot eh necessário
    inicializar os seguintes componentes:
    
    1. roscore : eh o ROS em sí
    2. rosbridge : middleware que faz a comunicação entre o ROS e
    aplicações "non-ROS". Disponibiliza um cliente javascript
    3. ardrone_autonomy: aplicação que junto com o ardrone_driver controla
    o AR.Drone Parrot.

---
+++ cg/script/init-ros.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function show_help {
+	echo "init-ros <option>"
+	echo "Option:"
+	echo "--roscore: Initializes roscore"
+	echo "--rosbridge: Initializes rosbridge_server and rosbridge_websocket"
+	echo "--rosrun: Initializes ardrone_autonomy and ardrone-driver"
+}
+
+
+if [ "$1" == "--roscore" ]; then
+	roscore
+elif [ "$1" == "--rosbridge" ]; then
+	roslaunch rosbridge_server rosbridge_websocket.launch
+elif [ "$1" == "--ardrone" ]; then
+	rosrun ardrone_autonomy ardrone_driver
+else
+	show_help
+fi
