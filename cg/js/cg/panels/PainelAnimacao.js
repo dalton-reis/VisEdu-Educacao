d7c96d3b33344de93795a13ac60a8521bc86d809
José Guilherme Vanz <guilherme.sft@gmail.com>
2015-03-30 22:53:22 -0300

    Painel animação - atualização de campos
    Foi adicionado no painel de animação campos que exibem os valores
    de posicionamento e rotação do object3D durante a animação.
    
    Foi incluso também um campo para configurar o tempo de duração
    de cada animação.

--- cg/js/cg/panels/PainelAnimacao.js
+++ cg/js/cg/panels/PainelAnimacao.js
@@ -17,7 +17,52 @@ function PainelAnimacao( editor ) {
 		throw new Error ( 'argumento deve ser da classe Editor !' );
 	}
 	scope.editor = editor;
-
+	//campos utilizados no painel
+	scope.positionX = new UI.Text('-').setColor('#666');
+	scope.positionY = new UI.Text('-').setColor('#666');
+	scope.positionZ = new UI.Text('-').setColor('#666');
+	scope.rotationX = new UI.Text('-').setColor('#666');
+	scope.rotationY = new UI.Text('-').setColor('#666');
+	scope.rotationZ = new UI.Text('-').setColor('#666');
+	scope.time = new UI.Number().setWidth('50px');
+	scope.time.setValue(2000);
+	//adiciona os campos para exibiram os valores do posicionamento do objeto
+	var linhaValues = new UI.Panel();
+	linhaValues.add(new UI.Text('Posição').setColor('#666'));
+	scope.add(linhaValues);
+	linhaValues = new UI.Panel();
+	linhaValues.add(new UI.Text('X: ').setColor('#666'));
+	linhaValues.add(scope.positionX);
+	scope.add(linhaValues);
+	linhaValues = new UI.Panel();
+	linhaValues.add(new UI.Text('Y: ').setColor('#666'));
+	linhaValues.add(scope.positionY);
+	scope.add(linhaValues);
+	linhaValues = new UI.Panel();
+	linhaValues.add(new UI.Text('Z: ').setColor('#666'));
+	linhaValues.add(scope.positionZ);
+	scope.add(linhaValues);
+	//adiciona os campos para exibiram os valores de rotação do objeto
+	linhaValues = new UI.Panel();
+	linhaValues.add(new UI.Text('Rotação').setColor('#666'));
+	scope.add(linhaValues);
+	linhaValues = new UI.Panel();
+	linhaValues.add(new UI.Text('X: ').setColor('#666'));
+	linhaValues.add(scope.rotationX);
+	scope.add(linhaValues);
+	linhaValues = new UI.Panel();
+	linhaValues.add(new UI.Text('Y: ').setColor('#666'));
+	linhaValues.add(scope.rotationY);
+	scope.add(linhaValues);
+	linhaValues = new UI.Panel();
+	linhaValues.add(new UI.Text('Z: ').setColor('#666'));
+	linhaValues.add(scope.rotationZ);
+	scope.add(linhaValues);
+	linhaValues = new UI.Panel();
+	linhaValues.add(new UI.Text('Time: ').setColor('#666'));
+	linhaValues.add(scope.time);
+	scope.add(linhaValues);
+	//botão que inicia as animações
 	var playButton = new UI.Button();
 	playButton.setLabel('Play!');
 	playButton.onClick( function () {
@@ -58,14 +103,20 @@ function PainelAnimacao( editor ) {
 								animation = new TWEEN.Tween(object3D.position)
 									.to({x: (animationItem.valorXYZ.x >= 0 ? "+" : "-") + Math.abs(animationItem.valorXYZ.x),
 									     y: (animationItem.valorXYZ.y >= 0 ? "+" : "-") + Math.abs(animationItem.valorXYZ.y),
-									     z: (animationItem.valorXYZ.z >= 0 ? "+" : "-") + Math.abs(animationItem.valorXYZ.z)}, 2000)
-									.easing(CG.getEasingFunction(easing));
+									     z: (animationItem.valorXYZ.z >= 0 ? "+" : "-") + Math.abs(animationItem.valorXYZ.z)}, scope.time.getValue())
+									.easing(CG.getEasingFunction(easing))
+									.onUpdate( function() {
+										updateValues(object3D);
+									});
 							} else if( animationItem.id == EIdsItens.ROTACIONAR ){
 								animation = new TWEEN.Tween(object3D.rotation)
 									.to({x: (animationItem.valorXYZ.x >= 0 ? "+" : "-") + Util.math.converteGrausParaRadianos(Math.abs(animationItem.valorXYZ.x)),
 									     y: (animationItem.valorXYZ.y >= 0 ? "+" : "-") + Util.math.converteGrausParaRadianos(Math.abs(animationItem.valorXYZ.y)),
-									     z: (animationItem.valorXYZ.z >= 0 ? "+" : "-") + Util.math.converteGrausParaRadianos(Math.abs(animationItem.valorXYZ.z))}, 2000)
-									.easing(CG.getEasingFunction(easing));
+									     z: (animationItem.valorXYZ.z >= 0 ? "+" : "-") + Util.math.converteGrausParaRadianos(Math.abs(animationItem.valorXYZ.z))}, scope.time.getValue())
+									.easing(CG.getEasingFunction(easing))
+									.onUpdate( function(){
+										updateValues(object3D);
+									});
 							}
 							animationChain.push(animation);
 						}
@@ -81,6 +132,18 @@ function PainelAnimacao( editor ) {
 				}//if
 			}//for
 		}
+	}
+
+	/**
+	 * Função para atualizar os valores do object3D durante a animação
+	 */
+	function updateValues( object3D ){
+		scope.positionX.setValue(object3D.position.x);
+		scope.positionY.setValue(object3D.position.y);
+		scope.positionZ.setValue(object3D.position.z);
+		scope.rotationX.setValue(Util.math.converteRadianosParaGraus(object3D.rotation.x));
+		scope.rotationY.setValue(Util.math.converteRadianosParaGraus(object3D.rotation.y));
+		scope.rotationZ.setValue(Util.math.converteRadianosParaGraus(object3D.rotation.z));
 	}
 }
 
