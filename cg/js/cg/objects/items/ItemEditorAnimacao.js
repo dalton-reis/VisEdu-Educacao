9c3935e432614f4e1b5214eb69939a0e388a73d2
José Guilherme Vanz <guilherme.sft@gmail.com>
2015-05-13 23:36:34 -0400

    Removendo linhas em branco
    Ajustado o encoding

--- cg/js/cg/objects/items/ItemEditorAnimacao.js
+++ cg/js/cg/objects/items/ItemEditorAnimacao.js
@@ -1,33 +1,25 @@
 /**
- * Item responsavel pela animaÁ„o do objeto gr·fico
+ * Item responsavel pela anima√ß√£o do objeto gr√°fico
  */
-
 ItemEditorAnimacao = function() {
-
 	AItemEditorEncaixeSeta.call( this );
-
 	var scope = this;
-
 	//eventos
-
 	//@Override
-	scope.onChange = function () {}; //evento ser· executado quando um filho for adicionado ou removido ou alguma propriedade for alterada
+	scope.onChange = function () {}; //evento ser√° executado quando um filho for adicionado ou removido ou alguma propriedade for alterada
 	//@Override
-	scope.onAddFilho = function ( item ) {};; //evento ser· executado quando um filho for adicionado
+	scope.onAddFilho = function ( item ) {};; //evento ser√° executado quando um filho for adicionado
 	//@Override
-	scope.onRemoveFilho = function ( item ) {};; //evento ser· executado quando um filho for removido
+	scope.onRemoveFilho = function ( item ) {};; //evento ser√° executado quando um filho for removido
 	//@Override
-	scope.onChangeFilhos = function ( filho ) {};; //evento ser· executado quando for removido ou inserido algum filho em um dos filhos do objeto, ou filho dos filhos e assim por diante
+	scope.onChangeFilhos = function ( filho ) {};; //evento ser√° executado quando for removido ou inserido algum filho em um dos filhos do objeto, ou filho dos filhos e assim por diante
 	//@Override
-	scope.afterChangeNome = function ( nomeAntigo ) {}; //evento ser· executado quando o nome do item for alterado
-
+	scope.afterChangeNome = function ( nomeAntigo ) {}; //evento ser√° executado quando o nome do item for alterado
 	//propriedades
 	scope.id =  EIdsItens.ANIMACAO
 	scope.corHex = CG.colors.corPecaIluminacao;
 	scope.gerarMeshsPecaSuperior();
-	/** FunÁ„o utilizada nas animaÁıes*/
+	/** Fun√ß√£o utilizada nas anima√ß√µes*/
 	scope.easing = CG.listaTiposEasing['Linear'];
-
 };
-
 ItemEditorAnimacao.prototype = Object.create( AItemEditorEncaixeSeta.prototype );
