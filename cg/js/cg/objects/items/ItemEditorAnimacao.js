5325ca04b76dea8b4bf4bd2946886c5ba03f50ac
José Guilherme Vanz <guilherme.sft@gmail.com>
2015-05-21 01:03:30 -0300

    Pequena mudança na documetação

--- cg/js/cg/objects/items/ItemEditorAnimacao.js
+++ cg/js/cg/objects/items/ItemEditorAnimacao.js
@@ -1,20 +1,20 @@
 /**
- * Item responsavel pela animação do objeto gráfico
+ * Item responsável pela animação do objeto gráfico
  */
 ItemEditorAnimacao = function() {
 	AItemEditorEncaixeSeta.call( this );
 	var scope = this;
 	//eventos
-	//@Override
-	scope.onChange = function () {}; //evento será executado quando um filho for adicionado ou removido ou alguma propriedade for alterada
-	//@Override
-	scope.onAddFilho = function ( item ) {}; //evento será executado quando um filho for adicionado
-	//@Override
-	scope.onRemoveFilho = function ( item ) {}; //evento será executado quando um filho for removido
-	//@Override
-	scope.onChangeFilhos = function ( filho ) {}; //evento será executado quando for removido ou inserido algum filho em um dos filhos do objeto, ou filho dos filhos e assim por diante
-	//@Override
-	scope.afterChangeNome = function ( nomeAntigo ) {}; //evento será executado quando o nome do item for alterado
+	//evento será executado quando um filho for adicionado ou removido ou alguma propriedade for alterada
+	scope.onChange = function () {}; 
+	//evento será executado quando um filho for adicionado
+	scope.onAddFilho = function ( item ) {}; 
+	//evento será executado quando um filho for removido
+	scope.onRemoveFilho = function ( item ) {}; 
+	//evento será executado quando for removido ou inserido algum filho em um dos filhos do objeto, ou filho dos filhos e assim por diante
+	scope.onChangeFilhos = function ( filho ) {}; 
+	//evento será executado quando o nome do item for alterado
+	scope.afterChangeNome = function ( nomeAntigo ) {}; 
 	//propriedades
 	scope.id =  EIdsItens.ANIMACAO
 	scope.corHex = CG.colors.corPecaIluminacao;
--- cg/js/cg/objects/items/ItemEditorTarget.js
+++ cg/js/cg/objects/items/ItemEditorTarget.js
@@ -5,19 +5,19 @@ function ItemEditorTarget() {
 	AItemEditorEncaixeQuadrado.call( this );
 	var scope = this;
 	//eventos
-	//@Override
+	//evento será executado quando um filho for adicionado ou removido ou alguma propriedade for alterada
 	scope.onChange = function () {
 		scope.object3D = createObject3D();
 		scope.object3D.item = scope;
-	}; //evento será executado quando um filho for adicionado ou removido ou alguma propriedade for alterada
-	//@Override
-	scope.onAddFilho = function ( item ) {}; //evento será executado quando um filho for adicionado
-	//@Override
-	scope.onRemoveFilho = function ( item ) {}; //evento será executado quando um filho for removido
-	//@Override
-	scope.onChangeFilhos = function ( filho ) {}; //evento será executado quando for removido ou inserido algum filho em um dos filhos do objeto, ou filho dos filhos e assim por diante
-	//@Override
-	scope.afterChangeNome = function ( nomeAntigo ) {}; //evento será executado quando o nome do item for alterado
+	}; 
+	//evento será executado quando um filho for adicionado
+	scope.onAddFilho = function ( item ) {}; 
+	//evento será executado quando um filho for removido
+	scope.onRemoveFilho = function ( item ) {}; 
+	//evento será executado quando for removido ou inserido algum filho em um dos filhos do objeto, ou filho dos filhos e assim por diante
+	scope.onChangeFilhos = function ( filho ) {}; 
+	//evento será executado quando o nome do item for alterado
+	scope.afterChangeNome = function ( nomeAntigo ) {}; 
 	//propriedades
 	scope.id =  EIdsItens.TARGET
 	scope.valorXYZ.set( 100, 100, 100 );
